e1440ab5af2fb26892fd8e954a42fa917e2716ba
汪心禾 <wangxinhe06@gmail.com>
2022-07-11 17:33:30 +0800

    Cache fetched Luogu records

--- src/sources/luogu/index.ts
+++ src/sources/luogu/index.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 import type { DataResponse, List, RecordBase } from "./api-docs/luogu-api";
 
-const getRecords = (user: string, page = 1): Promise<RecordBase[]> =>
+const cache: Record<number, RecordBase[]> = {};
+
+const getRecordPage = (
+  user: number,
+  page: number,
+  cached: RecordBase[]
+): Promise<RecordBase[]> =>
   axios
     .get<DataResponse<{ records: List<RecordBase> }>>(
       `https://www.luogu.com.cn/record/list?user=${user}&page=${page}`,
@@ -15,10 +21,20 @@ const getRecords = (user: string, page = 1): Promise<RecordBase[]> =>
     .then(({ data }) => data.currentData.records)
     .then(async (records) => {
       if (records.result.length < records.perPage!) return records.result;
-      return records.result.concat(await getRecords(user, page + 1));
+      const index = cached
+        .map(({ id }) => id)
+        .indexOf(records.result[records.result.length - 1].id);
+      if (index >= 0) return records.result.concat(cached.slice(index + 1));
+      return records.result.concat(await getRecordPage(user, page + 1, cached));
     });
 
-getRecords("108135")
+const getRecords = async (user: number) => {
+  const records = await getRecordPage(user, 1, cache[user] ?? []);
+  cache[user] = records;
+  return records;
+};
+
+getRecords(108135)
   .then((records) => records.map(({ submitTime }) => submitTime))
   .then((t) => t.map((time) => new Date(time * 1000)))
   .then(console.log)
