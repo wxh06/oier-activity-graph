9e26f4ed771141c3c3e57c9fd3cd854723f016d3
汪心禾 <wangxinhe06@gmail.com>
2022-07-17 23:06:53 +0800

    Relative activity level

--- src/utils.ts
+++ src/utils.ts
@@ -1,30 +1,39 @@
 import { DateTime } from "luxon";
 
-export type Level = 0 | 1 | 2 | 3 | 4 | 5;
-
 export interface DailyData {
   x: number;
   y: number;
   date: DateTime;
   count: number;
-  level: Level;
+  level: number;
 }
 
 export function groupByWeek(
   activities: Record<string, number>,
   f: DateTime,
-  t: DateTime
+  t: DateTime,
+  maxLevel = 4
 ) {
   const weeks: DailyData[][] = [];
+  let max = 0;
+  Object.entries(activities).forEach(([date, count]) => {
+    const dateTime = DateTime.fromISO(date);
+    if (dateTime >= f.startOf("day") && dateTime <= t.endOf("day"))
+      max = count > max ? count : max;
+  });
   for (
     let date = f.startOf("day"), week: DailyData[] = [];
     date <= t;
     date = date.plus({ days: 1 })
   ) {
     const count = activities[date.toISODate()] ?? 0;
-    let level: Level = Math.ceil(count / 5) as never;
-    if (level > 5) level = 5;
-    week.push({ x: weeks.length, y: date.weekday % 7, date, count, level });
+    week.push({
+      x: weeks.length,
+      y: date.weekday % 7,
+      date,
+      count,
+      level: max ? Math.ceil((count / max) * maxLevel) : 0,
+    });
     if (date.weekday === 6 || date.equals(t.startOf("day"))) {
       weeks.push(week);
       week = [];
